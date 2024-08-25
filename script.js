d3ba69e11a3399877e15e2577bb49f86fc12c085
MaxHopf <81119053+MaxHopf@users.noreply.github.com>
2024-08-25 22:34:55 +0300

    Add dialog UI allowing the user to enter new book data
    for adding a new book the user is required to enter book data like
    title, author, page numer and read status

--- script.js
+++ script.js
@@ -1,3 +1,30 @@
 'use strict'
 
 console.log('Hello from JavaSript console!');
+
+const btnToShowAddBookDialog = document.querySelector('[data-show-add-book-dialog-btn]');
+const addBookDialog = document.querySelector('[data-add-book-dialog]');
+const btnToCloseAddBookDialog = addBookDialog.querySelector('[data-close-add-book-dialog]');
+const btnToSaveAddBookDialogFormInputs = addBookDialog.querySelector('[data-save-add-book-dialog-form-inputs]');
+const exitBtn = addBookDialog.querySelector('[data-exit-btn]');
+
+
+exitBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    addBookDialog.close();
+});
+
+btnToShowAddBookDialog.addEventListener("click", () => {
+  addBookDialog.showModal();
+});
+
+btnToCloseAddBookDialog.addEventListener("click", (e) => {
+  e.preventDefault();
+  addBookDialog.close();
+});
+
+btnToSaveAddBookDialogFormInputs.addEventListener("click", (e) => {
+    e.preventDefault();
+    console.log('save user input');
+    addBookDialog.close();
+  });
